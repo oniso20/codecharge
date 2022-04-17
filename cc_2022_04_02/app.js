68747add35ecfbf0fdc9129cdc39ac08145bbfc3
Onis Emem <emem.c.onis@gmail.com>
2022-04-17 18:11:58 +0300

    Final working solution added

--- cc_2022_04_02/app.js
+++ cc_2022_04_02/app.js
@@ -5,4 +5,12 @@
 // 348597 => [7,9,5,8,4,3]
 // 0 => [0]
 
-function reverseNum
+//I need to .split(''), then .reverse() but they only work on strings
+//I convert to string first with .toString()
+//Numbers were still string... I used .map to produce a new array which didn't work until I multiplied the item by 1 (item*1)
+
+function reverseNum(r){
+    return r.toString().split('').reverse().map(item => item * 1)
+  }
+  
+  console.log(reverseNum(348597))
