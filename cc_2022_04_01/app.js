e257772955dd0e1bf509acb0832d548953de11d8
Onis Emem <emem.c.onis@gmail.com>
2022-04-18 20:55:44 +0300

    Changes made to  cc_2022_04_01/ file name

---
+++ cc_2022_04_01/app.js
@@ -0,0 +1,15 @@
+// Given an array of integers, return a new array with each value doubled.
+
+// For example:
+
+// [1, 2, 3] --> [2, 4, 6]
+
+//Make a function that takes in an array
+//map through the array, double each element (*2) and return the resulting array
+
+function doubles(array){
+    return array.map(item=>item*2)
+  }
+
+  //Test
+  doubles([1,2,3])
